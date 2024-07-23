011bc3e96b2425e9cfc6cec8f42782c765cf3292
yeju874 <kyj960@gmail.com>
2024-07-23 09:39:05 +0900

    함수 연습 완료

---
+++ demofilter.py
@@ -0,0 +1,14 @@
+#Demofilter.py
+
+lst = [10,25,30]
+iterL = filter(None,lst)
+for item in iterL:
+    print("Item:{0}".format(item))
+
+def getBiggerThan20(i):
+    return i>20
+
+iterL = filter(getBiggerThan20,lst)
+for item in iterL:
+    print("Item:{0}".format(item))
+
