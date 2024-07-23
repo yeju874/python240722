e81c09158a1ed0321c360101897777b627536be9
yeju874 <kyj960@gmail.com>
2024-07-23 09:26:30 +0900

    함수 연습

--- demoLoop.py
+++ demoLoop.py
@@ -8,3 +8,11 @@ while value > 0:
 lst = ["apple", 10, 10.5]
 for item in lst:
     print(item)
+
+# 리스트 함축 연습
+lst = [1,2,3,4,5]
+print([i**2 for i in lst if i<4])
+t = ("apple","banana","orange")
+print([len(i) for i in t])
+d = {100:"apple",200:"banana",300:"orange"}
+print([v.upper() for v in d.values()])
---
+++ practice.py
@@ -0,0 +1,24 @@
+#연습
+a = "python"
+print(a[0:3])
+
+colors = ["red","yellow","pink"]
+colors.append("white")
+print(colors)
+colors.insert(1,"black")
+print(colors)
+colors.remove("red")
+print(colors)
+def times(a,b):
+    return a*b
+
+print(times(1,2))
+
+listrange=list(range(10))
+print(listrange)
+
+listA = list(range(10,1,-2))
+print(listA)
+
+
+
